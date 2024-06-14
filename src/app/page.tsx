e04f52f6ff33d54ae6679b3f543b282d9fbed952
Shaaz <asadakhtar800@gmail.com>
2024-06-14 16:09:15 +0530

    Design Navbar with Acertinity UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 export default function Home() {
   return (
-   <h1>Hello , Shaaz Welcome to NEXT JS</h1>
+   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <h1 className="text-2xl text-center">Welcome to The Website</h1>
+   </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
 
-const Navbar = () => {
+
+
+const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
   return (
-    <div>Navbar</div>
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+
+      <Menu setActive={setActive}>
+        <Link href={'/'}
+        >
+
+          <MenuItem setActive={setActive} active={active} item="Home">
+
+          </MenuItem>
+        </Link>
+
+        <MenuItem setActive={setActive} active={active} item="Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/hobby">All courses</HoveredLink>
+            <HoveredLink href="/individual">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/team">Advanced Composition</HoveredLink>
+            <HoveredLink href="/enterprise">Songwriting</HoveredLink>
+          </div>
+        </MenuItem>
+
+
+        <Link href={'/contact'}
+        >
+
+          <MenuItem setActive={setActive} active={active} item="Contact Us">
+
+          </MenuItem>
+        </Link>
+
+
+
+      </Menu>
+    </div>
   )
 }
 
